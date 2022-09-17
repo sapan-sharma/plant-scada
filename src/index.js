3b865c82bee93bd60eb29c9a5a1313ebb7c71dc5
sapan sharma <sapan.sharma9@gmail.com>
2022-09-17 20:32:11 +0530

    SCADAX first release

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import App from './App'
-import './index.css'
-import {ContextProvider} from './contexts/ContextProvider'
+import './index.css';
+import App from './App';
+import { ContextProvider } from './contexts/ContextProvider';
 
 ReactDOM.render(
-	<ContextProvider><App/></ContextProvider>, document.getElementById('root') )
+  <React.StrictMode>
+    <ContextProvider>
+      <App />
+    </ContextProvider>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
